c8ddb1c23ff22a5c89d8f3d5e586b84ae1553ce5
Daniel Hahler <git@thequod.de>
2015-10-05 12:14:01 +0200

    Covered test

--- tests.py
+++ tests.py
@@ -7,6 +7,9 @@ class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(awesome.smile(), ":)")
 
+    def test_new(self):
+        self.assertEqual(awesome.test(), "test")
+
 
 if __name__ == '__main__':
     unittest.main()
