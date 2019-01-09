76cf63501628eec3b450290b0fcf3019f9f13b1f
Thiago Ribeiro Ramos <thiago@ribeiroramos.com>
2019-01-09 19:36:23 -0200

    AAAA

--- awesome/__init__.py
+++ awesome/__init__.py
@@ -1,5 +1,12 @@
 def smile():
     return ":)"
 
+
 def frown():
     return ":("
+
+
+def fib(n):
+    if n < 2:
+        return 1
+    return fib(n) + fib(n-1)
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,3 +3,7 @@ import awesome
 
 def test_something():
     assert awesome.smile() == ':)'
+
+
+def test_fib():
+    assert awesome.fib(1) == 1
