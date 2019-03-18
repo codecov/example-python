91575554ab4c0ed4f37600ac4ae4a42a8aa9c95b
Shabbir Akolawala <shabbir.akolawala@dubizzle.com>
2019-03-18 10:07:20 +0400

    add the new test for the coverage

--- tests.py
+++ tests.py
@@ -7,6 +7,8 @@ class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(awesome.smile(), ":)")
 
+     def test_frown(self):
+        self.assertEqual(awesome.frown, ":(")
 
 if __name__ == '__main__':
     unittest.main()
