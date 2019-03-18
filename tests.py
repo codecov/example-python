7c1bf0519cc0eadb58166bc868b6d39847d1130a
Shabbir Akolawala <shabbir.akolawala@dubizzle.com>
2019-03-18 10:23:11 +0400

    added a dumb test to PR

--- tests.py
+++ tests.py
@@ -10,5 +10,8 @@ class TestMethods(unittest.TestCase):
     def test_frown(self):
         self.assertEqual(awesome.frown(), ":(")
 
+    def test_dumb(self):
+        self.assertEqual("apple", "apple")
+
 if __name__ == '__main__':
     unittest.main()
