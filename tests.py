99e9b359a7fab4e985e776dd391d5d6dfd002ceb
Shabbir Akolawala <shabbir.akolawala@dubizzle.com>
2019-03-18 10:08:09 +0400

    fix the indentation

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(awesome.smile(), ":)")
 
-     def test_frown(self):
+    def test_frown(self):
         self.assertEqual(awesome.frown, ":(")
 
 if __name__ == '__main__':
