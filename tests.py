665d87c12e78e7f82cde889fb7802cf60d0753be
xfhc@9495@163.com <xfhc9495@163.com>
2020-03-09 15:51:26 +0800

    for build

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ import awesome
 class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(awesome.smile(), ":)")
-
+        print()
 
 if __name__ == '__main__':
     unittest.main()
