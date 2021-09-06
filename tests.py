b0aa3ee401cfdd8fcbbfe7ce69cdb7a689988130
glozanoa <glozanoa@uni.pe>
2021-09-06 18:06:53 -0500

    update tests

--- tests.py
+++ tests.py
@@ -6,6 +6,8 @@ import awesome
 class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(awesome.smile(), ":)")
+    def test_sub(self):
+        self.assertEqual(awesome.frown(), ":(")
 
 
 if __name__ == '__main__':
