032678d95e3a75e3b6c4a32c36147028bee67452
Tom Hu <tom@codecov.io>
2022-09-22 14:05:22 -0400

    test: make change

--- app/calculator.py
+++ app/calculator.py
@@ -12,9 +12,3 @@ class Calculator:
         if y == 0:
             return 'Cannot divide by 0'
         return x * 1.0 / y
-
-    def power(x, y):
-        return x**y
-
-    def one():
-        return 1
