75d78e1729592cbd5750616b1f992c22b76e0f79
Dana <dana@codecov.io>
2022-10-13 11:00:33 +0300

    remove second test

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -16,12 +16,12 @@ def test_subtract():
     assert Calculator.subtract(2.0, 0.0) == 2.0
     assert Calculator.subtract(-4, 2.0) == -6.0
 
-def test_multiply():
-    assert Calculator.multiply(1, 2) == 2.0
-    assert Calculator.multiply(1.0, 2.0) == 2.0
-    assert Calculator.multiply(0, 2.0) == 0.0
-    assert Calculator.multiply(2.0, 0.0) == 0.0
-    assert Calculator.multiply(-4, 2.0) == -8.0
+# def test_multiply():
+#     assert Calculator.multiply(1, 2) == 2.0
+#     assert Calculator.multiply(1.0, 2.0) == 2.0
+#     assert Calculator.multiply(0, 2.0) == 0.0
+#     assert Calculator.multiply(2.0, 0.0) == 0.0
+#     assert Calculator.multiply(-4, 2.0) == -8.0
 
 # def test_divide():
 #     # assert Calculator.divide(1, 2) == 0.5
