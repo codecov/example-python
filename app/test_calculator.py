ff68c0b422592d9d72677fbe1748b3022327c228
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2023-04-24 15:29:47 +0200

    Investigating  CODE-3043
    Ticket: [CODE-3043](https://codecovio.atlassian.net/browse/CODE-3043)

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -1,13 +1,6 @@
 from .calculator import Calculator
 
 
-def test_add():
-    assert Calculator.add(1, 2) == 3.0
-    assert Calculator.add(1.0, 2.0) == 3.0
-    assert Calculator.add(0, 2.0) == 2.0
-    assert Calculator.add(2.0, 0) == 2.0
-    assert Calculator.add(-4, 2.0) == -2.0
-
 def test_subtract():
     assert Calculator.subtract(1, 2) == -1.0
     assert Calculator.subtract(2, 1) == 1.0
@@ -22,10 +15,3 @@ def test_multiply():
     assert Calculator.multiply(0, 2.0) == 0.0
     assert Calculator.multiply(2.0, 0.0) == 0.0
     assert Calculator.multiply(-4, 2.0) == -8.0
-
-def test_divide():
-    # assert Calculator.divide(1, 2) == 0.5
-    assert Calculator.divide(1.0, 2.0) == 0.5
-    assert Calculator.divide(0, 2.0) == 0
-    assert Calculator.divide(-4, 2.0) == -2.0
-    # assert Calculator.divide(2.0, 0.0) == 'Cannot divide by 0'
