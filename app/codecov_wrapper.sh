aa56693445cc749b20e4d9342a3a1deb3aea63b7
Dana <dana@codecov.io>
2023-07-20 14:43:49 +0300

    test bazel

---
+++ app/codecov_wrapper.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+# `bazel coverage --run_under foo :bar` basically translates to:
+#     foo pytest --cov bar/src/a.py bar/src/b.py
+# We want to run that `pytest` command unmodified, as below:
+"$@"
+pyenv virtualenv codecov-env
+pyenv activate codecov-env
+pip install codecov-cli
+# Now we want to run `codecovcli`
+# CODECOV_TOKEN=<hardcoded per project :(> codecovcli create-commit
+# CODECOV_TOKEN=<hardcoded per project :(> codecovcli create-report
+# CODECOV_TOKEN=<hardcoded per project :(> codecovcli do-upload
