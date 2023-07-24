a5c27188f1c474fcdfdb7c7cdcd198486514f952
Dana <dana@codecov.io>
2023-07-24 13:38:29 +0300

    hide token

--- app/codecov_wrapper.sh
+++ app/codecov_wrapper.sh
@@ -4,17 +4,13 @@
 #     foo pytest --cov bar/src/a.py bar/src/b.py
 # We want to run that `pytest` command unmodified, as below:
 "$@"
-#COVERAGE_DIR="$CURR_DIR/bazel-out/_coverage"
-# Now you can use $COVERAGE_DIR to access the coverage report files
-# For example, if you want to print the coverage report path:
-echo "Coverage report path: $COVERAGE_DIR"
-# Assuming you are in the root of your workspace
+
+# codecov doesn't recognise pylcov.dat as a coverage report, renaming it to lcov.dat so codecov can acknowledge it  
 mv $COVERAGE_DIR/pylcov.dat $COVERAGE_DIR/lcov.dat
 
-# Get the root of the Git repository
-GIT_DIR="$GIT_DIR"
-codecovcli -v create-commit -t 02d15256-c911-4a8e-a642-1685b111da77
-codecovcli -v create-report -t 02d15256-c911-4a8e-a642-1685b111da77
-codecovcli -v do-upload -t 02d15256-c911-4a8e-a642-1685b111da77 --plugin gcov --plugin pycoverage -s $COVERAGE_DIR
+# uploading coverage
+codecovcli -v --url http://localhost:8000 create-commit -t $CODECOV_TOKEN
+codecovcli -v --url http://localhost:8000 create-report -t $CODECOV_TOKEN
+codecovcli -v --url http://localhost:8000 do-upload -t $CODECOV_TOKEN -s $COVERAGE_DIR
 
 
