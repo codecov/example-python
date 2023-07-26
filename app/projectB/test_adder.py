a94d303684be4b49d83abd8d52cc04bcbf0446d4
Dana <dana@codecov.io>
2023-07-26 12:59:54 +0300

    fix adder tests

--- app/projectB/test_adder.py
+++ app/projectB/test_adder.py
@@ -4,8 +4,8 @@ from app.projectB.adder import Adder
 
 
 class TestAdder(unittest.TestCase):
-    def test_adder(self, x, y):
-        assert x + y == Adder.add(x, y)
+    def test_adder(self):
+        assert 2 + 3 == Adder().add(2, 3)
 
 if __name__ == "__main__":
     unittest.main()
