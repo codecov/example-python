e6c160713289a6fad3b77f048a9d958e73b6b005
Dana <dana@codecov.io>
2023-07-26 12:52:06 +0300

    run adder tests

--- app/projectB/test_adder.py
+++ app/projectB/test_adder.py
@@ -3,7 +3,9 @@ import unittest
 from app.projectB.adder import Adder
 
 
-
 class TestAdder(unittest.TestCase):
     def test_adder(self, x, y):
         assert x + y == Adder.add(x, y)
+
+if __name__ == "__main__":
+    unittest.main()
