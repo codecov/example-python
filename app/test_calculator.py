dc143914579726e3a3f8d9901debf4b90ce19ee3
Rohan Bhaumik <rohanbhaumik@FVFHQ2MKQ05P.local>
2023-09-25 09:50:19 -0400

    removing exponent function

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -30,6 +30,3 @@ def test_divide():
     assert Calculator.divide(-4, 2.0) == -2.0
     assert Calculator.divide(2.0, 0.0) == 'Cannot divide by 0'
     assert Calculator.divide(3.0, 0.0) == 'Cannot divide by 0'
-
-def test_exponent():
-    assert Calculator.exponent(2.0,2.0) == 4.0
