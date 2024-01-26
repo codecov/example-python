de91c244e6e9fde106166b744ab09587c4f179ce
Gguidini <giovanni@codecov.io>
2024-01-26 09:01:24 -0300

    add new ops: sqrt and pow

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -1,3 +1,4 @@
+import pytest
 from .calculator import Calculator
 
 
@@ -29,3 +30,19 @@ def test_divide():
     assert Calculator.divide(0, 2.0) == 0
     assert Calculator.divide(-4, 2.0) == -2.0
     # assert Calculator.divide(2.0, 0.0) == 'Cannot divide by 0'
+
+def test_sqrt():
+    assert Calculator.sqrt(100) == pytest.approx(10)
+    assert Calculator.sqrt(4) == pytest.approx(2)
+    assert Calculator.sqrt(2) == pytest.approx(1.4142, 1e-3)
+    assert Calculator.sqrt(1) == pytest.approx(1)
+    assert Calculator.sqrt(100, 0.0000001) == pytest.approx(10)
+    assert Calculator.sqrt(2, 0.01) == pytest.approx(1.414, 1e-2)
+    assert Calculator.sqrt(2, 0.0000001) == pytest.approx(1.41421356237, 1e-7)
+
+def test_pow():
+    assert Calculator.pow(2, 2) == 4
+    assert Calculator.pow(1, 2) == 1
+    assert Calculator.pow(4, 2) == 16
+    assert Calculator.pow(3, 3) == 27
+    assert Calculator.pow(10, 5) == 100000
