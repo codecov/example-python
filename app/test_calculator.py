ca571f8dc9d64269ce400fdf48e641b07fced71c
JerrySentry <jerry.feng@sentry.io>
2024-01-29 18:04:08 -0500

    add a unit test assertion

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -29,3 +29,4 @@ def test_divide():
     assert Calculator.divide(0, 2.0) == 0
     assert Calculator.divide(-4, 2.0) == -2.0
     # assert Calculator.divide(2.0, 0.0) == 'Cannot divide by 0'
+    assert Calculator.divide(2.0, 4.0) == 0.5
