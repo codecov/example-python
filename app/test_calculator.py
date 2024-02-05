afb4b31ce58805bbc0f9e17f07a095d944732f75
Tom Hu <thomas.hu@sentry.io>
2024-02-05 11:11:42 -0800

    fix: fail test

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -2,7 +2,7 @@ from .calculator import Calculator
 
 
 def test_add():
-    assert Calculator.add(1, 2) == 3.0
+    assert Calculator.add(1, 2) == 5.0
     assert Calculator.add(1.0, 2.0) == 3.0
     assert Calculator.add(0, 2.0) == 2.0
     assert Calculator.add(2.0, 0) == 2.0
